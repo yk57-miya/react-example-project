d8a4c76afb46075f49ee5c720f311ff09dfe013b
miyashitayuki <ykm0402.myshi@gmail.com>
2020-01-06 19:28:26 +0900

    ボタン実装

--- src/pages/root/index.js
+++ src/pages/root/index.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import Button from '../../components/Button';
 
 export default class Page1 extends Component {
   constructor (props) {
     super(props);
-    this.str = 'TOPページ';
+    this.str = 'Hello World!!!';
+    this.introductionList = [ 'about', 'profile', 'works' ];
   }
   render () {
     return (
-      <div>
-        { this.str }
+      <div className="Root">
+        <h1 className="Root__Title">{ this.str }</h1>
+        <div className="Root__Contents">
+          <Button myDate={ this.introductionList } />
+        </div>
       </div>
     );
   }
