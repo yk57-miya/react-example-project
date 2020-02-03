596bd2d609575650a60172b2c0b274820522dc8b
miyashitayuki <ykm0402.myshi@gmail.com>
2020-02-03 19:09:29 +0900

    ボタン実装とtabの修正

--- src/pages/root/index.js
+++ src/pages/root/index.js
@@ -5,14 +5,34 @@ export default class Page1 extends Component {
   constructor (props) {
     super(props);
     this.str = 'Hello World!!!';
-    this.introductionList = [ 'about', 'profile', 'works' ];
+    this.buttons = [
+      {
+        label: 'about',
+        to: '/about'
+      },
+      {
+        label: 'profile',
+        to: '/profile'
+      },
+      {
+        label: 'works',
+        to: '/works'
+      }
+    ];
   }
   render () {
     return (
       <div className="Root">
         <h1 className="Root__Title">{ this.str }</h1>
         <div className="Root__Contents">
-          <Button myDate={ this.introductionList } />
+          <div className="Root__Buttons">
+            {
+              this.buttons.map((item, i) => {
+                return <Button key={ i } item={ item } />;
+              })
+            }
+            {/* <Button myDate={ this.introductionList } /> */}
+          </div>
         </div>
       </div>
     );
